19b0ef61ce9e5b0693bfbc39edcb8c6cdb282e90
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2024-02-29 11:16:08 +0000

    Added a few more examples of joins and filters

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ album = db.Table(
 metadata_obj.create_all(engine)
 
 with engine.connect() as conn:
-    # insert_cmd = sa.insert(album).values(
-    #         name="Violator",
-    #         artist="Depeche Mode"
-    #     )
+    # We have to add items individually if we want to know their autoincrement key
+    # There is no workaround for this
 
     item1_result = conn.execute(
         db.insert(artist).values(artist_name="Depeche Mode")
     )
+
+    # Notice that the result inserted_primary_key is a tuple, so the "," is really important
 
     item1_id, = item1_result.inserted_primary_key
 
@@ -45,12 +45,16 @@ with engine.connect() as conn:
 
     item3_id, = item3_result.inserted_primary_key
 
+    # Inserting one album, using the artist id received above
+
     first_album = conn.execute(
         album.insert().values(
             album_name="Speak and Spell",
             artist_id=item1_id
         )
     )
+
+    # Inserting many albums
 
     insert_albums = conn.execute(
         db.insert(album),
@@ -61,8 +65,41 @@ with engine.connect() as conn:
         ]
     )
 
+    # Now let's get some information using a join
+
     query_result = conn.execute(
-        db.select(album.c.album_name, artist.c.artist_name).select_from(db.join(artist, album))
+        db.select(db.join(artist, album))
+    )
+
+    for row in query_result:
+        print(row)
+
+    # Another approach to getting information using a join
+    # Note that we already declared the Foreign Keys so this "just works"
+
+    query_result = conn.execute(
+        db.select(artist.join(album))
+    )
+
+    for row in query_result:
+        print(row)
+
+    # Now let's get some information using a join but restricting the columns
+    # The ".c" part is selecting columns - I always think it looks like magic
+
+    query_result = conn.execute(
+        db.select(album.c.album_name, artist.c.artist_name).select_from(artist.join(album))
+    )
+
+    for row in query_result:
+        print(row)
+
+    # And now let's restrict the list using a filter - time to move to multiple lines methinks...
+
+    query_result = conn.execute(
+        db.select(album.c.album_name)
+        .select_from(artist.join(album))
+        .filter(artist.c.artist_name == "Depeche Mode")
     )
 
     for row in query_result:
