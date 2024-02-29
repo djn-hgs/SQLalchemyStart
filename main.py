a360cebaa49022210a8beb63ca68ea67dba379bc
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2024-02-29 12:32:16 +0000

    Now a single table using ORM

--- main.py
+++ main.py
@@ -1,51 +1,58 @@
 import sqlalchemy as db
+import sqlalchemy.orm as orm
 
 engine = db.create_engine("sqlite+pysqlite:///albums.sqlite", echo=True)
 
-metadata_obj = db.MetaData()
+# We no longer need a metadata object
+# Now we just create an abstract Base class which has its own metadata
 
-album = db.Table(
-    "album",
-    metadata_obj,
-    db.Column("id", db.Integer, primary_key=True),
-    db.Column("name", db.String),
-    db.Column("artist", db.String)
-)
+class Base(orm.DeclarativeBase):
+    pass
 
-metadata_obj.create_all(engine)
+# A slight tweak here - we declare column names and used mapped columns
+# There is more to come here
 
-with engine.connect() as conn:
-    # insert_cmd = sa.insert(album).values(
-    #         name="Violator",
-    #         artist="Depeche Mode"
-    #     )
+class Album(Base):
+    __tablename__ = "album"
 
-    insert_result = conn.execute(
-        db.insert(album),
+    id = orm.mapped_column(db.Integer, primary_key=True)
+    name = orm.mapped_column(db.String)
+    artist = orm.mapped_column(db.String)
+
+# Now the metadata is created for our Base class
+
+Base.metadata.create_all(engine)
+
+# We now use a Session rather than a connection
+
+with orm.Session(engine) as session:
+    # So to add to our table we can now just create an instance and add it
+
+    new_album = Album(name="A Broken Frame", artist="Depeche Mode")
+
+    session.add(new_album)
+
+    # And now we can query it "session.scalars" is always a bit odd, i think
+
+    for album in session.scalars(db.select(Album)):
+        print(f'{album.name} by {album.artist}')
+
+    # And to add many items
+
+    session.add_all(
         [
-            {
-                "name": "Violator",
-                "artist": "Depeche Mode"
-            }
+            Album(name="Violator", artist="Depeche Mode"),
+            Album(name="Orbital", artist="Orbital"),
+            Album(name="Technique", artist="New Order")
         ]
     )
 
-    # print(insert_result.inserted_primary_key)
+    # Don't forget to commit
 
-    insert_more = conn.execute(
-        db.insert(album),
-        [
-            {"name": "Orbital", "artist": "Orbital"},
-            {"name": "Technique", "artist": "New Order"}
-        ]
-    )
+    session.commit()
 
-    query_result = conn.execute(
-        db.select(album)
-    )
+    artist_query = db.select(Album).where(Album.artist=="Depeche Mode")
 
-    for row in query_result:
-        print(row)
-
-    conn.commit()
+    for album in session.scalars(artist_query):
+        print(f'{album.name} by {album.artist}')
 
