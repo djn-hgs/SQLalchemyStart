b60aad97a767f87a13d1cd8240062289fb62a0c2
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2024-02-29 00:03:47 +0000

    Two tables with a foreign key

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import sqlalchemy as sqla
+import sqlalchemy as db
 
-engine = sqla.create_engine("sqlite+pysqlite:///albums.sqlite", echo=True)
+engine = db.create_engine("sqlite+pysqlite:///albums.sqlite", echo=True)
 
-metadata_obj = sqla.MetaData()
+metadata_obj = db.MetaData()
 
-album = sqla.Table(
+artist = db.Table(
+    "artist",
+    metadata_obj,
+    db.Column("artist_id", db.Integer, primary_key=True),
+    db.Column("artist_name", db.String),
+)
+
+album = db.Table(
     "album",
     metadata_obj,
-    sqla.Column("id", sqla.Integer, primary_key=True),
-    sqla.Column("name", sqla.String),
-    sqla.Column("artist", sqla.String)
+    db.Column("id", db.Integer, primary_key=True),
+    db.Column("album_name", db.String),
+    db.Column("artist_id", db.String, db.ForeignKey("artist.artist_id"))
 )
 
 metadata_obj.create_all(engine)
@@ -20,28 +27,42 @@ with engine.connect() as conn:
     #         artist="Depeche Mode"
     #     )
 
-    insert_result = conn.execute(
-        sqla.insert(album),
-        [
-            {
-                "name": "Violator",
-                "artist": "Depeche Mode"
-            }
-        ]
+    item1_result = conn.execute(
+        db.insert(artist).values(artist_name="Depeche Mode")
     )
 
-    # print(insert_result.inserted_primary_key)
+    item1_id, = item1_result.inserted_primary_key
 
-    insert_more = conn.execute(
-        sqla.insert(album),
+    item2_result = conn.execute(
+        db.insert(artist).values(artist_name="Orbital")
+    )
+
+    item2_id, = item2_result.inserted_primary_key
+
+    item3_result = conn.execute(
+        db.insert(artist).values(artist_name="New Order")
+    )
+
+    item3_id, = item3_result.inserted_primary_key
+
+    first_album = conn.execute(
+        album.insert().values(
+            album_name="Speak and Spell",
+            artist_id=item1_id
+        )
+    )
+
+    insert_albums = conn.execute(
+        db.insert(album),
         [
-            {"name": "Orbital", "artist": "Orbital"},
-            {"name": "Technique", "artist": "New Order"}
+            {"album_name": "Violator", "artist_id": item1_id},
+            {"album_name": "Orbital", "artist_id": item2_id},
+            {"album_name": "Technique", "artist_id": item3_id}
         ]
     )
 
     query_result = conn.execute(
-        sqla.select(album)
+        db.select(album.c.album_name, artist.c.artist_name).select_from(db.join(artist, album))
     )
 
     for row in query_result:
