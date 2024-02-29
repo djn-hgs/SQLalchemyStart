671ab4e97c22044dbb1ce3d99633e5b6ad1006d3
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2024-02-29 11:49:45 +0000

    Added comments and discovered a pycharm bug

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ with engine.connect() as conn:
 
     artist3_id, = artist3_result.inserted_primary_key
 
-    # Now some albums
+    # Now some albums - we need to add individually if we want to associate a genre
 
     album1_result = conn.execute(
         album.insert().values(
@@ -73,11 +73,20 @@ with engine.connect() as conn:
 
     album1_id, = album1_result.inserted_primary_key
 
+    album2_result = conn.execute(
+        album.insert().values(
+            album_name="Orbital",
+            artist_id=artist2_id
+        )
+    )
+
+    album2_id, = album2_result.inserted_primary_key
+
     insert_albums = conn.execute(
         db.insert(album),
         [
             {"album_name": "Violator", "artist_id": artist1_id},
-            {"album_name": "Orbital", "artist_id": artist2_id},
+            {"album_name": "Orbital 2", "artist_id": artist2_id},
             {"album_name": "Technique", "artist_id": artist3_id}
         ]
     )
@@ -106,11 +115,12 @@ with engine.connect() as conn:
         album_genre.insert(),
         [
             {"album_id": album1_id, "genre_id": genre1_id},
-            {"album_id": album1_id, "genre_id": genre2_id}
+            {"album_id": album1_id, "genre_id": genre2_id},
+            {"album_id": album2_id, "genre_id": genre1_id}
         ]
     )
 
-    # And a query
+    # A simple query using a join
 
     artist_album_result = conn.execute(
         db.select(album.c.album_name, artist.c.artist_name)
@@ -123,6 +133,8 @@ with engine.connect() as conn:
     for row in artist_album_result:
         print(row)
 
+    # Now let's use our link table in the join
+
     artist_album_genre_result = conn.execute(
         db.select(album.c.album_name, artist.c.artist_name, genre.c.genre_name)
         .select_from(
@@ -134,6 +146,41 @@ with engine.connect() as conn:
     )
 
     for row in artist_album_genre_result:
+        print(row)
+
+    # And now let's throw in a filter as well
+
+    query_result = conn.execute(
+        db.select(album.c.album_name, artist.c.artist_name, genre.c.genre_name)
+        .select_from(
+            artist
+            .join(album)
+            .join(album_genre)
+            .join(genre)
+        )
+        .filter(artist.c.artist_name == 'Depeche Mode')
+    )
+
+    for row in query_result:
+        print(row)
+
+    # Or how about this? Filter and where do exactly the same job
+
+    query_result = conn.execute(
+        db.select(album.c.album_name, artist.c.artist_name, genre.c.genre_name)
+        .select_from(
+            artist
+            .join(album)
+            .join(album_genre)
+            .join(genre)
+        )
+        .where(genre.c.id==genre1_id)
+    )
+
+    # There is a Pycharm bug in the .where clause
+    # https://github.com/sqlalchemy/sqlalchemy/issues/9337
+
+    for row in query_result:
         print(row)
 
     conn.commit()
